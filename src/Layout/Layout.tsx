0b97a5a868a970ec0c0b655a37f3bee496e85266
Saif Ur Rehman <saifurreham58@gmail.com>
2025-09-19 18:05:45 +0500

    add table data
    19/9/25

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { AppSidebar } from "@/components/app-sidebar"
-
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
@@ -7,20 +6,37 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { Outlet } from "react-router-dom"
+import { Power } from "lucide-react" 
 export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
+        <header className="flex h-16 shrink-0 items-center justify-between border-b px-4 bg-[#4F5467]">
+          
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="text-white"/>
+            <Separator orientation="vertical" className="mr-2 h-4 " />
            
           </div>
+
+         
+          <div className="flex items-center gap-4">
+            <button
+              onClick={() => {
+                localStorage.removeItem("token") 
+                window.location.href = "/login" 
+              }}
+              className="p-2 rounded-full cursor-pointer  text-[#FFFFFF] mr-3.5"
+              title="Logout"
+            >
+              <Power size={22} />
+            </button>
+          </div>
         </header>
+
         <main className="flex-1 p-4 bg-[#EDF1F5]">
-<Outlet/>
+          <Outlet />
         </main>
       </SidebarInset>
     </SidebarProvider>
--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,22 +1,34 @@
-import { Routes, Route } from "react-router-dom";
-// import App from "@/App";
-// import { LoginSection } from "@/app/LoginPage/LoginPage";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "@/Layout/Layout";
-// import  Home  from "@/app/HomePage/Home";
-import  MyLeaves  from "@/app/MyLeavesPage/MyLeaves";
+import {MyLeaves} from "@/app/MyLeavesPage/MyLeaves";
 import { Login } from "@/app/LoginPage/Login";
-
+import Home from "@/app/HomePage/Home";
 
 function AppRouter() {
+  const isLoggedIn = !!localStorage.getItem("token"); // true/false
+
   return (
     <Routes>
-      {/* <Route path="/" element={<App />} /> */}
-      {/* <Route path="/login" element={<LoginSection />} /> */}
-      <Route element={<Layout/>}>
-        <Route path="/" element={<h1>Hello World</h1>} />
-      <Route path="/Home" element={<Login />} />
-      <Route path="/MyLeaves" element={<MyLeaves />} />
-      </Route>
+     
+      {!isLoggedIn && (
+        <Route path="/login" element={<Login />} />
+      )}
+
+      
+      {isLoggedIn && (
+        <Route element={<Layout />}>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/MyLeaves" element={<MyLeaves />} />
+        </Route>
+      )}
+
+      
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? <Navigate to="/Home" /> : <Navigate to="/login" />
+        }
+      />
     </Routes>
   );
 }
